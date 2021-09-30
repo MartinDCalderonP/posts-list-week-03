bf71d496db435940a3f5729e71df58339ed2e2c1
Martín Calderón <martincalderon92@outlook.com>
2021-09-30 15:41:49 -0300

    :recycle: Move fetchs to helper functions file.

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -1,3 +1,27 @@
+import { tagsUrl, postsUrl } from './commonVariables.js';
+
+export function getTags(successFunction) {
+	fetch(tagsUrl)
+		.then((data) => data.json())
+		.then(successFunction)
+		.catch((err) => {
+			console.log(
+				`El servidor devolvió un error: ${err}. <br> Inténtelo de nuevo más tarde.`
+			);
+		});
+}
+
+export function getPosts(successFunction) {
+	fetch(postsUrl)
+		.then((data) => data.json())
+		.then(successFunction)
+		.catch((err) => {
+			console.log(
+				`El servidor devolvió un error: ${err}. <br> Inténtelo de nuevo más tarde.`
+			);
+		});
+}
+
 export function sortArrayByDate(a, b) {
 	return new Date(a.createDate) - new Date(b.createDate);
 }
