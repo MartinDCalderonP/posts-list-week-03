e347b70823671168afd6522ad5a4f9dbdf1ae3a3
Martín Calderón <martincalderon92@outlook.com>
2021-09-29 22:36:02 -0300

    :sparkles: Add show feature section function.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 'use strict';
 
-import { getTags } from './fetcher.js';
+import { getTags, getLastThreePosts } from './fetcher.js';
 
+let featureSection = document.getElementById('featureSection');
 let chipsList = document.getElementById('chipsList');
 
 getTags(showChips);
+getLastThreePosts(showFeatureSection);
 
 function showChips(tags) {
 	for (let i = 0; i < tags.length; i++) {
 		chipsList.innerHTML += `<div class="chips-container__chips-list__chip">${tags[i].name}</div>`;
+	}
+}
+
+function showFeatureSection(posts) {
+	for (let i = 0; i < 3; i++) {
+		featureSection.innerHTML += `<div class="feature-section__card">
+										<h1>${posts[i].title}</h1>
+										<h2>${posts[i].subTitle}</h2>
+										<img src=${posts[i].image}>
+										<p>${posts[i].body}</p>
+										<p>${posts[i].createDate}</p>
+										<p>Likes: ${posts[i].likes}</p>
+										<p>Author: ${posts[i].author}</p>
+										<p>Tags: ${posts[i].tags}</p>
+									</div>`;
 	}
 }
