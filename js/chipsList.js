6439e8440d0b68e16e0c915124207119306a250e
Martín Calderón <martincalderon92@outlook.com>
2021-09-30 18:24:36 -0300

    :recycle: Refactor filter by tag function.

--- js/chipsList.js
+++ js/chipsList.js
@@ -18,10 +18,9 @@ function showChips(tags) {
 function filterByTag(e) {
 	if (e.target.className.match('list__chip')) {
 		let filteredPosts;
+		let id = parseInt(e.target.id);
 
-		filteredPosts = posts.filter((item) =>
-			item.tags.includes(parseInt(e.target.id))
-		);
+		filteredPosts = posts.filter((item) => item.tags.includes(id));
 
 		showCards(filteredPosts);
 	}
