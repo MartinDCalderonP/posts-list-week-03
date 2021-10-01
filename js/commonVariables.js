d8d4d40baad7bdc46031e31411fe70152103c3ce
Martín Calderón <martincalderon92@outlook.com>
2021-10-01 18:14:11 -0300

    :art: Add body to common variables.

--- js/commonVariables.js
+++ js/commonVariables.js
@@ -3,6 +3,7 @@ export const postsUrl = serverUrl + '/posts';
 export const tagsUrl = serverUrl + '/tags';
 export const authorsUrl = serverUrl + '/authors';
 
+export const body = document.getElementsByTagName('body');
 export const chipsList = document.getElementById('chipsList');
 export const featureSection = document.getElementById('featureSection');
 export const regularSection = document.getElementById('regularSection');
