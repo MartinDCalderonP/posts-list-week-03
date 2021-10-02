f454a6a3c1c082ee48e04fe1784b10c0b40f998d
Martín Calderón <martincalderon92@outlook.com>
2021-10-02 16:57:58 -0300

    ✨Add get create date and name to slug functions.

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -45,3 +45,22 @@ export function getAuthorName(id) {
 	let fullName = `${author.name} ${author.lastName}`;
 	return fullName;
 }
+
+export function getCreateDate() {
+	let date = new Date();
+
+	let day = String(date.getDate()).padStart(2, '0');
+	let month = String(date.getMonth() + 1).padStart(2, '0');
+	let year = date.getFullYear();
+
+	let result = `${year}/${month}/${day}`;
+
+	return result;
+}
+
+export function nameToSlug(string) {
+	let lowerCasedString = string.toLowerCase();
+	let result = lowerCasedString.replace(' ', '-');
+
+	return result;
+}
